97d4d9d7a34d0de2f6f270707149833d7bbd715e
Saidaziz007 <saidaminovvs@gmail.com>
2023-12-11 01:03:49 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,271 @@
+// Boolean1
+
+// let A = 2;
+
+// let res = A % 2 == 0;
+
+// console.log(res);
+
+// Boolean2
+
+// let A = 2;
+// let B = -5;
+
+// let res = A >= 0;
+// let res1 = B < -2;
+
+// console.log(res);
+// console.log(res1);
+
+// Boolean3
+
+// let A = 1;
+// let B = 2;
+// let C = 3;
+
+// let res = A < B < C;
+
+// console.log(res);
+
+// Boolean4
+
+// let A = 3;
+// let B = 5;
+
+// let res = A % 2 == 1;
+// let res1 = B % 2 == 1;
+
+// console.log(res);
+// console.log(res1);
+
+// Boolean5
+
+// let A = 2;
+// let B = 3;
+
+// let res = A % 2 !== 0;
+// let res1 = B % 2 !== 0;
+
+// console.log(res);
+// console.log(res1);
+
+// Boolean6
+
+// let A = 7;
+// let B = 4;
+
+// let res = A % 2 !== 0;
+// let res1 = B % 2 !== 0;
+
+// console.log(res);
+// console.log(res1);
+
+// Boolean7
+
+// let A = 5;
+// let B = 6;
+// let C = 10;
+
+// let res = A > 0;
+// let res1 = B > 0;
+// let res2 = C > 0;
+
+// console.log(res);
+// console.log(res1);
+// console.log(res2);
+
+// Boolean8
+
+// let A = 5;
+// let B = 6;
+// let C = 10;
+
+// let res = A > 0;
+// let res1 = B < 0;
+// let res2 = C < 0;
+
+// console.log(res);
+// console.log(res1);
+// console.log(res2);
+
+// Boolean9
+
+// let A = 5;
+// let B = 6;
+// let C = 10;
+
+// let res = A > 0;
+// let res1 = B > 0;
+// let res2 = C < 0;
+
+// console.log(res);
+// console.log(res1);
+// console.log(res2);
+
+// Boolean10
+
+// let ab = 12;
+
+// let res = ab % 2 == 0;
+
+// console.log(res);
+
+// Boolean11
+
+// let abc = 123;
+
+// let res = abc % 2 == 1;
+
+// console.log(res);
+
+// Boolean12
+
+// let = A = 1;
+// let = B = 2;
+// let = C = -1;
+
+// let res = (Math.abs(A) === Math.abs(B) && Math.sign(A) !== Math.sign(B))||(
+//   Math.abs(A) === Math.abs(C) && Math.sign(A) !== Math.sign(C)
+// )||(Math.abs(C) === Math.abs(B) && Math.sign(C) !== Math.sign(B));
+
+// console.log(res);
+
+// Boolean13
+
+// let A = 123;
+
+// let res1 = A % 10;
+// let res2 = (A % 100 - res1) / 10;
+// let res3 = (A - res2 *10 - res1) / 100;
+
+// let res = ((res3 < res2) && (res2 < res1));
+
+// console.log(res);
+
+// Boolean14
+
+// let A = 123;
+
+// let res1 = A % 10;
+// let res2 = (A % 100 - res1) / 10;
+// let res3 = (A - res2 *10 - res1) / 100;
+
+// let res = ((res3 < res2) && (res2 < res1)) || ((res3 > res2) && (res2 > res1));
+
+// console.log(res);
+
+// Boolean15
+
+// let abc = 121;
+
+// let c = abc % 10;
+// let b = (abc % 100 - c) / 10;
+// let a = (abc - b *10 - c) / 100;
+
+// let res = a == c;
+
+// console.log(res);
+
+// Operators1
+
+// let A = 5;
+// let B = 8;
+
+// let Y = (-B / 2) * (Math.pow(A, 2) + Math.pow(B, 2));
+
+// console.log(Y);
+
+// Operators2
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+// let D = 6;
+
+// let Y = Math.pow((A + B / C + D), 2) + Math.pow(B, 2);
+
+// console.log(Y);
+
+// Operators3
+
+// let A = 2;
+// let C = 5;
+// let D = 3;
+
+// let Y = 2 * (Math.pow(A, D) + (4 * Math.pow(C, 2)) / 3);
+
+// console.log(Y);
+
+// Operators4
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+// let D = 6;
+
+// let Y = 1 / Math.pow(A , 2) * (B / 10) ** 3 * (C + D) ** 2;
+
+// console.log(Y);
+
+// Operators5
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+// let D = 6;
+
+// let Y = Math.pow((((A / B) - 1) / (C / (D - 1))) ,2);
+
+// console.log(Y);
+
+// Operators6
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+// let D = 6;
+
+// let Y = ((A + B) / (C + D / (A + C)));
+
+// console.log(Y);
+
+// Operators7
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+
+// let Y = Math.sqrt(Math.pow(A + B + C, 2) - Math.pow(A - B - C, 2));
+
+// console.log(Y);
+
+// Operators8
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+
+// let Y = ((Math.pow(A , 2) + Math.pow(B , 2) + Math.pow(C , 2)) / (A * B * C));
+
+// console.log(Y);
+
+// Operators9
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+// let D = 6;
+
+// let Y = A * (Math.pow(D, 3) / 3) + B * (Math.pow(C, 2) / 2);
+
+// console.log(Y);
+
+// Operators10
+
+// let A = 3;
+// let B = 4;
+// let C = 5;
+
+// let Y = Math.abs(A / B + B / C) + Math.pow((A + C), 1 / 4);
+
+// console.log(Y);
